14a4a52c149e423875b8dddadfd34e28bd616e3e
Kuberan J <91178809+kuberan35@users.noreply.github.com>
2024-07-31 14:47:17 +0530

    Update addToCart.js

--- frontend/src/helpers/addToCart.js
+++ frontend/src/helpers/addToCart.js
@@ -1,50 +1,131 @@
-import SummaryApi from "../common"
-import { toast } from 'react-toastify'
+import React, { useContext, useState } from 'react'
+import loginIcons from '../assest/signin.gif'
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { Link, useNavigate } from 'react-router-dom';
+import SummaryApi from '../common';
+import { toast } from 'react-toastify';
+import Context from '../context';
 
-const addToCart = async(e,id) =>{
-    e?.stopPropagation()
-    e?.preventDefault()
+const Login = () => {
+    const [showPassword,setShowPassword] = useState(false)
+    const [data,setData] = useState({
+        email : "",
+        password : ""
+    })
+    const navigate = useNavigate()
+    const { fetchUserDetails, fetchUserAddToCart } = useContext(Context)
 
-    let token = localStorage.getItem('token');
+    const handleOnChange = (e) =>{
+        const { name , value } = e.target
+
+        setData((preve)=>{
+            return{
+                ...preve,
+                [name] : value
+            }
+        })
+    }
+
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+
+        let token = localStorage.getItem('token');
   let gettoken = JSON.parse(token);
 
-    const response = await fetch(SummaryApi.addToCartProduct.url,{
-        method : SummaryApi.addToCartProduct.method,
-        credentials : 'include',
-        headers : {
-            "content-type" : 'application/json',
-            Authorization: gettoken,
-        },
-        body : JSON.stringify(
-            { productId : id }
-        )
-    })
+        const dataResponse = await fetch(SummaryApi.signIn.url,{
+            method : SummaryApi.signIn.method,
+            credentials : 'include',
+            headers : {
+                "content-type" : "application/json",
+                Authorization: gettoken,
+            },
+            body : JSON.stringify(data)
+        })
 
-    const responseData = await response.json()
+        const dataApi = await dataResponse.json()
 
-    if(responseData.success){
-        toast.success(responseData.message)
+        if(dataApi.success){
+            toast.success(dataApi.message)
+            console.log(dataApi)
+            localStorage.setItem("token",JSON.stringify(dataApi.data))
+            navigate('/')
+            fetchUserDetails()
+            fetchUserAddToCart()
+        }
+
+        if(dataApi.error){
+            toast.error(dataApi.message)
+        }
+
     }
 
-    if(responseData.error){
-        toast.error(responseData.message)
-    }
+    console.log("data login",data)
+    
+  return (
+    <section id='login'>
+        <div className='mx-auto container p-4'>
+
+            <div className='bg-white p-5 w-full max-w-sm mx-auto'>
+                    <div className='w-20 h-20 mx-auto'>
+                        <img src={loginIcons} alt='login icons'/>
+                    </div>
+
+                    <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+                        <div className='grid'>
+                            <label>Email : </label>
+                            <div className='bg-slate-100 p-2'>
+                                <input 
+                                    type='email' 
+                                    placeholder='enter email' 
+                                    name='email'
+                                    value={data.email}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'/>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label>Password : </label>
+                            <div className='bg-slate-100 p-2 flex'>
+                                <input 
+                                    type={showPassword ? "text" : "password"} 
+                                    placeholder='enter password'
+                                    value={data.password}
+                                    name='password' 
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'/>
+                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
+                                    <span>
+                                        {
+                                            showPassword ? (
+                                                <FaEyeSlash/>
+                                            )
+                                            :
+                                            (
+                                                <FaEye/>
+                                            )
+                                        }
+                                    </span>
+                                </div>
+                            </div>
+                            <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-red-600'>
+                                Forgot password ?
+                            </Link>
+                        </div>
+
+                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+
+                    </form>
+
+                    <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className=' text-red-600 hover:text-red-700 hover:underline'>Sign up</Link></p>
+            </div>
 
 
-    return responseData
-
+        </div>
+    </section>
+  )
 }
 
-// let token = localStorage.getItem('token');
-//   let gettoken = JSON.parse(token);
-//   const requestOptions = {
-//     method: 'GET',
-//     mode: 'cors',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: gettoken,
-//     },
-//   };
-//   const response = await fetch(`${BaseUrl}/api1/message/getallmessages?chatId=${chatId}&limit=${limit}&page=${page}&offset=${offset}`, requestOptions);
-
-export default addToCart
+export default Login
