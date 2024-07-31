662f59c4db9e33e72e66da23772a43c3d04652ae
Kuberan J <91178809+kuberan35@users.noreply.github.com>
2024-07-31 14:51:22 +0530

    Update userSignIn.js

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -35,7 +35,7 @@ async function userSignInController(req,res){
             secure : true
         }
 
-        res.cookie("token",token,tokenOption).status(200).json({
+        res.status(200).json({
             message : "Login successfully",
             data : token,
             success : true,
