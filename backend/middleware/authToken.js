25abed98a78ed32e46318e7a2d928e71fd335c66
Kuberan J <91178809+kuberan35@users.noreply.github.com>
2024-07-31 14:48:17 +0530

    Update authToken.js

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -2,9 +2,8 @@ const jwt = require('jsonwebtoken')
 
 async function authToken(req,res,next){
     try{
-        const token = req.cookies?.token
-
-        console.log("token",token)
+        const token = req.headers.authorization;
+       console.log("token",token)
         if(!token){
             return res.status(200).json({
                 message : "Please Login...!",
@@ -26,6 +25,10 @@ async function authToken(req,res,next){
             next()
         });
 
+//         const crypto = require('crypto');
+// const secret = crypto.randomBytes(64).toString('hex');
+// console.log(secret);
+
 
     }catch(err){
         res.status(400).json({
@@ -39,3 +42,54 @@ async function authToken(req,res,next){
 
 
 module.exports = authToken
+
+
+// const jwt = require('jsonwebtoken');
+
+// async function authToken(req, res, next) {
+//     try {
+//         const authHeader = req.headers.authorization;
+
+//         if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//             return res.status(401).json({
+//                 message: "Please Login...!",
+//                 error: true,
+//                 success: false
+//             });
+//         }
+
+//         const token = authHeader.split(' ')[1]; // Extract token from 'Bearer <token>'
+//         console.log("Token:", token);
+
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "Token is missing",
+//                 error: true,
+//                 success: false
+//             });
+//         }
+
+//         jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
+//             if (err) {
+//                 console.log("Token verification error:", err);
+//                 return res.status(401).json({
+//                     message: "Invalid token",
+//                     error: true,
+//                     success: false
+//                 });
+//             }
+
+//             req.userId = decoded._id;
+//             next();
+//         });
+//     } catch (err) {
+//         res.status(400).json({
+//             message: err.message || err,
+//             data: [],
+//             error: true,
+//             success: false
+//         });
+//     }
+// }
+
+// module.exports = authToken;
